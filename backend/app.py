16f44caa7d95015f33331c7b8d0b17b12b333688
qduydo <jackydo1974@gmail.com>
2025-01-19 09:37:58 -0800

    use .env instead of secrets.py

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,14 @@ from pymongo import MongoClient
 from controllers.scheduleController import schedule_blueprint
 from controllers.mapController import map_endpoints  # Import map_endpoints
 from flask_cors import CORS
+import os
   # Allow all origins, or specify your frontend URL
 
-import my_secrets
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +19,7 @@ def create_app():
     # ---------------------------------------------------------------
     # 1. MongoDB Connection (moved from model.py to app.py)
     # ---------------------------------------------------------------
-    MONGO_URI = my_secrets.MONGO_URI
+    MONGO_URI =  os.environ.get("MONGO_URI")
     client = MongoClient(MONGO_URI)
     db = client.my_database
     app.config['DB'] = db  # store db in app config (or globally)
